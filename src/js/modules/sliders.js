a7219df58cf66f3406c2cb7073afb75760220ebe
Dmytro <ignatikd0@gmail.com>
2025-04-19 16:39:53 +0300

    partners

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,140 +1,162 @@
 import Swiper from 'swiper'
-import {Autoplay, Navigation, Thumbs} from 'swiper/modules'
-import {getElement, getElements} from '../core/index.js'
-
+import { Autoplay, Navigation, Thumbs } from 'swiper/modules'
+import { getElement, getElements } from '../core/index.js'
 
 function aboutUsSliderNav() {
-    if (!getElement('[data-swiper="aboutUsSliderNav"]')) return
+	if (!getElement('[data-swiper="aboutUsSliderNav"]')) return
 
-    return new Swiper('[data-swiper="aboutUsSliderNav"]', {
-        modules: [Navigation],
-        spaceBetween: 24,
-        slidesPerView: 2,
-        allowTouchMove: false,
-        initialSlide: 1,
-        slideToClickedSlide: true,
-        on: {
-            slideChange: function () {
-                const slides = this.slides;
-                slides.forEach((slide, index) => {
-                    if (index === this.activeIndex) {
-                        slide.classList.add('swiper-slide-hidden');
-                    } else {
-                        slide.classList.remove('swiper-slide-hidden');
-                    }
-                });
-            },
-        }
-    })
+	return new Swiper('[data-swiper="aboutUsSliderNav"]', {
+		modules: [Navigation],
+		spaceBetween: 24,
+		slidesPerView: 2,
+		allowTouchMove: false,
+		initialSlide: 1,
+		slideToClickedSlide: true,
+		on: {
+			slideChange: function () {
+				const slides = this.slides
+				slides.forEach((slide, index) => {
+					if (index === this.activeIndex) {
+						slide.classList.add('swiper-slide-hidden')
+					} else {
+						slide.classList.remove('swiper-slide-hidden')
+					}
+				})
+			},
+		},
+	})
 }
 
 function aboutUsSlider() {
-    const mainSlider = getElement('[data-swiper="aboutUsSlider"]');
-    if (!mainSlider) return;
-    const mainSlides = mainSlider.querySelectorAll('.swiper-slide');
-    const isEven = mainSlides.length % 2 === 0;
-    const emptySlidesCount = isEven ? 1 : 2;
-    const swiperWrapper = getElement('[data-swiper="aboutUsSliderNav"] .swiper-wrapper');
-    for (let i = 0; i < emptySlidesCount; i++) {
-        const emptySlide = document.createElement('div');
-        emptySlide.classList.add('swiper-slide', 'swiper-slide-empty');
-        swiperWrapper.append(emptySlide);
-    }
-    const swiperNav = aboutUsSliderNav();
-    new Swiper('[data-swiper="aboutUsSlider"]', {
-        modules: [Navigation, Thumbs],
-        spaceBetween: 32,
-        slidesPerView: 1,
-        thumbs: {
-            swiper: aboutUsSliderNav(),
-        },
-        navigation: {
-            nextEl: '.aboutUsSlid-next',
-            prevEl: '.aboutUsSlid-prev',
-        },
-        on: {
-            slideChange: function () {
-                // Сдвиг навигационного слайдера при переключении основного
-                swiperNav.slideTo(this.activeIndex + 1);
-            },
-        },
-
-    })
+	const mainSlider = getElement('[data-swiper="aboutUsSlider"]')
+	if (!mainSlider) return
+	const mainSlides = mainSlider.querySelectorAll('.swiper-slide')
+	const isEven = mainSlides.length % 2 === 0
+	const emptySlidesCount = isEven ? 1 : 2
+	const swiperWrapper = getElement(
+		'[data-swiper="aboutUsSliderNav"] .swiper-wrapper'
+	)
+	for (let i = 0; i < emptySlidesCount; i++) {
+		const emptySlide = document.createElement('div')
+		emptySlide.classList.add('swiper-slide', 'swiper-slide-empty')
+		swiperWrapper.append(emptySlide)
+	}
+	const swiperNav = aboutUsSliderNav()
+	new Swiper('[data-swiper="aboutUsSlider"]', {
+		modules: [Navigation, Thumbs],
+		spaceBetween: 32,
+		slidesPerView: 1,
+		thumbs: {
+			swiper: aboutUsSliderNav(),
+		},
+		navigation: {
+			nextEl: '.aboutUsSlid-next',
+			prevEl: '.aboutUsSlid-prev',
+		},
+		on: {
+			slideChange: function () {
+				// Сдвиг навигационного слайдера при переключении основного
+				swiperNav.slideTo(this.activeIndex + 1)
+			},
+		},
+	})
 }
 
 function newsSlider() {
-    if (!getElement('[data-swiper="newsSlider"]')) return
-    new Swiper('[data-swiper="newsSlider"]', {
-        modules: [Navigation],
-        spaceBetween: 24,
-        slidesPerView: 1,
-        breakpoints: {
-            640: {
-                slidesPerView: 2,
-            },
-            1024: {
-                slidesPerView: 4,
-            },
-        },
-        navigation: {
-            nextEl: '.newsSlid-next',
-            prevEl: '.newsSlid-prev',
-        },
-    })
+	if (!getElement('[data-swiper="newsSlider"]')) return
+	new Swiper('[data-swiper="newsSlider"]', {
+		modules: [Navigation],
+		spaceBetween: 24,
+		slidesPerView: 1,
+		breakpoints: {
+			640: {
+				slidesPerView: 2,
+			},
+			1024: {
+				slidesPerView: 4,
+			},
+		},
+		navigation: {
+			nextEl: '.newsSlid-next',
+			prevEl: '.newsSlid-prev',
+		},
+	})
 }
 
 function volunteersSlider() {
-    if (!getElement('[data-swiper="volunteersSlider"]')) return
-    new Swiper('[data-swiper="volunteersSlider"]', {
-        modules: [Navigation],
-        spaceBetween: 24,
-        slidesPerView: 1,
-        loop: true,
-        breakpoints: {
-            1024: {
-                slidesPerView: 2,
-            },
-        },
-        navigation: {
-            nextEl: '.newsSlid-next',
-            prevEl: '.newsSlid-prev',
-        },
-    })
+	if (!getElement('[data-swiper="volunteersSlider"]')) return
+	new Swiper('[data-swiper="volunteersSlider"]', {
+		modules: [Navigation],
+		spaceBetween: 24,
+		slidesPerView: 1,
+		loop: true,
+		breakpoints: {
+			1024: {
+				slidesPerView: 2,
+			},
+		},
+		navigation: {
+			nextEl: '.newsSlid-next',
+			prevEl: '.newsSlid-prev',
+		},
+	})
 }
 
 function ourTeamSlider() {
-    if (!getElement('[data-swiper="ourТeamSlider"]')) return
-    getElements('[data-swiper="ourТeamSlider"]').forEach(item => {
-
-        new Swiper(item, {
-            modules: [Navigation, Autoplay],
-            spaceBetween: 24,
-            slidesPerView: 1,
-            breakpoints: {
-                640: {
-                    slidesPerView: 2,
-                },
-                1024: {
-                    slidesPerView: 4,
-                },
-            },
-            autoplay: {
-                delay: 5000,
-                disableOnInteraction: false,
-            },
-            navigation: {
-                nextEl: getElement('.ourТeamSlider-next', item),
-                prevEl: getElement('.ourТeamSlider-prev', item),
-            },
-        })
-    })
-
+	if (!getElement('[data-swiper="ourТeamSlider"]')) return
+	getElements('[data-swiper="ourТeamSlider"]').forEach(item => {
+		new Swiper(item, {
+			modules: [Navigation, Autoplay],
+			spaceBetween: 24,
+			slidesPerView: 1,
+			breakpoints: {
+				640: {
+					slidesPerView: 2,
+				},
+				1024: {
+					slidesPerView: 4,
+				},
+			},
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: false,
+			},
+			navigation: {
+				nextEl: getElement('.ourТeamSlider-next', item),
+				prevEl: getElement('.ourТeamSlider-prev', item),
+			},
+		})
+	})
+}
+function partnersSlider() {
+	if (!getElement('[data-swiper="partnersSlider"]')) return
+	getElements('[data-swiper="partnersSlider"]').forEach(item => {
+		new Swiper(item, {
+			modules: [Autoplay],
+			spaceBetween: 32,
+			slidesPerView: 2,
+			loop: true,
+			speed: 5000,
+			breakpoints: {
+				1024: {
+					slidesPerView: 5,
+				},
+			},
+			autoplay: {
+				delay: 0,
+				disableOnInteraction: false,
+			},
+			freeMode: true, // включає режим без “щаблів” — плавна прокрутка
+			freeModeMomentum: false,
+		})
+	})
 }
 
 export {
-    aboutUsSlider,
-    aboutUsSliderNav,
-    newsSlider,
-    volunteersSlider,
-    ourTeamSlider,
+	aboutUsSlider,
+	aboutUsSliderNav,
+	newsSlider,
+	volunteersSlider,
+	ourTeamSlider,
+	partnersSlider,
 }
